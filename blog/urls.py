8406968b1b4102e65b2665c8377da222579006e1
Fernando Robles <luis.fer1407@gmail.com>
2026-07-07 15:41:51 -0600

    Creacion del crud para la aplicacion de categorias

--- blog/urls.py
+++ blog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from categories.urls import router_categories
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -36,6 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
+    path('api/', include(router_categories.urls)),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
---
+++ categories/views.py
@@ -0,0 +1,7 @@
+from rest_framework.viewsets import ModelViewSet
+from categories.models import Category
+from categories.serializers import CategorySerializer
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()

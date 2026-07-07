f3c7828df1e663933e1dd8a7cc09078930eaf820
Fernando Robles <luis.fer1407@gmail.com>
2026-07-07 15:52:05 -0600

    Aniadiendo permisos a los endpoints de categorias, todo el muendo puede leer, pero para modificar, eliminar o crear solo sean usuarios del staff

--- categories/views.py
+++ categories/views.py
@@ -1,7 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from categories.models import Category
 from categories.serializers import CategorySerializer
+from categories.permissions import IsAdminOrReadOnly
 
 class CategoryViewSet(ModelViewSet):
+
+    permission_classes = [IsAdminOrReadOnly]
+
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
